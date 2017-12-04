ac0685a02878cd7d150b57a0a5a4ab0ab6cc50a5
TToiv <toit1_15@uni.worc.ac.uk>
2017-12-04 10:17:56 +0000

    ghjueghsegiohf

--- js/index.js
+++ js/index.js
@@ -34,10 +34,10 @@ function createDialog() {
 	//here's a simple example
       
 	navigator.notification.confirm(
-    	'What do you think of this dialog?',  // message
+    	'Are you tired?',  // message
         dialogDismissed,         // callback
-        'An example dialog!',            // title
-        ['Awesome!', 'Sucks']                  // buttons
+        'How bored are you?',            // title
+        ['Yes!', 'No']                  // buttons
     );
 
 }
@@ -46,8 +46,8 @@ function createDialog() {
         	
 function dialogDismissed(buttonIndex) {
 	
-	if(buttonIndex==1) new Toast({content: "You're easily pleased", duration: 3000});
-   	else if(buttonIndex==2) new Toast({content: 'It is rather boring.', duration: 3000});
+	if(buttonIndex==1) new Toast({content: "Take a break and eat some food.", duration: 3000});
+   	else if(buttonIndex==2) new Toast({content: 'Keep working.', duration: 4000});
 
 }
 
