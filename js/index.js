5f5db13b1dd885f679a2d57da24ff395881c53ba
TToiv <toit1_15@uni.worc.ac.uk>
2017-12-04 11:23:01 +0000

    asdsadsad

--- js/index.js
+++ js/index.js
@@ -64,7 +64,7 @@ function createNotification() {
     //setup notification
     //
     
-    cordova.plugins.notification.local.schedule({ 
+    cordova.plugins.notification.local.schedule({
     	id: 		1,
         title: 		"Hey you",
         message: 	"This is an example notification",
